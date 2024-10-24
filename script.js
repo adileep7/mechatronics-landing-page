744e08727a6ef225b3fdcf8f8739b708db2ed048
adileep7 <51901681+adileep7@users.noreply.github.com>
2024-10-24 15:35:54 -0700

    Add files via upload
    Added all files

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+    alert('Thank you for applying!'); // Simple feedback
+});
